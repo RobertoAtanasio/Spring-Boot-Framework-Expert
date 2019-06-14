28fdc0472c52c1b9e2864f630e9bd410a5f3df04
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-06-14 10:20:29 -0300

    Criando classes conversoras para datas

--- src/main/java/com/algaworks/brewer/repository/listener/CervejaEntityListener.java
+++ src/main/java/com/algaworks/brewer/repository/listener/CervejaEntityListener.java
@@ -2,8 +2,6 @@ package com.algaworks.brewer.repository.listener;
 
 import javax.persistence.PostLoad;
 
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-
 import com.algaworks.brewer.BrewerApplication;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.storage.FotoStorage;
@@ -21,7 +19,7 @@ public class CervejaEntityListener {
 	
 	@PostLoad
 	public void postLoad(final Cerveja cerveja) {
-		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+//		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 		FotoStorage fotoStorage = BrewerApplication.getBean(FotoStorage.class);
 		cerveja.setUrlFoto(fotoStorage.getUrl(cerveja.getFotoOuMock()));
 		cerveja.setUrlThumbnailFoto(fotoStorage.getUrl(FotoStorage.THUMBNAIL_PREFIX + cerveja.getFotoOuMock()));
